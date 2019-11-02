98d3fb793d637e6e9d9191881d2fd7214195212a
Adithya188288 <adithya.santhosh33@gmail.com>
2019-11-02 20:24:00 +0530

    Need to add a environment variable local for local development

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,21 @@
 const express = require("express")
-const router = express.Router();
-
+const router = express.Router()
 
 //api whoami
-router.get("/", function(req,res){
+router.get("/", function(req, res) {
+  let ipaddress = ""
+  // x-forwarded-for response header will be set by the server . so for local dev set a contant
+  if (process.env.NODE_ENV === "local") {
+    ipaddress = "127.0.0.1"
+  } else {
+    ipaddress = req.headers["x-forwarded-for"].slice(0, 12)
+  }
+  let software = req.headers["user-agent"]
+  let language = req.headers["accept-language"]
 
-  let ipaddress = req.headers['x-forwarded-for'].slice(0,12);
-  let software = req.headers['user-agent']
-  let language = req.headers['accept-language']
-  
-  const result = {ipaddress,language,software}
-  
+  const result = { ipaddress, language, software }
+
   res.send(result)
 })
-
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,17 +2,18 @@
 // where your node app starts
 
 // init project
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
 
 //require all the startup files. all files in the startup folder are IIFE
-require("./startup/cors.js")(app);
-require("./startup/routes.js")(app);
+require("./startup/cors.js")(app)
+require("./startup/routes.js")(app)
+
+const PORT = process.env.PORT || 3000
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function() {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+var listener = app.listen(PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port)
+})
 
-
-//Completed Test cases.All Okay:) 
+//Completed Test cases.All Okay:)
